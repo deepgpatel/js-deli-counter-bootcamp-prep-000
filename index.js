81a026be9c3917463825de00ffc70abac534f6c0
Deep Patel <deepgpatel93@gmail.com>
2018-02-07 20:35:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var katzDeli = [];
 
 function takeANumber(katzDeli, newPerson){
-  var katzDeliLine = [katzDeli];
-  katzDeliLine.push
+  katzDeli.push(newPerson)
   // var i = 0;
   // //for (i = 0; i < katzDeliLine.length; i++) {
   //   katzDeliLine.push("Welcome,"+ newPerson +". You are number"+ katzDeliLine.length  +"in line.")
