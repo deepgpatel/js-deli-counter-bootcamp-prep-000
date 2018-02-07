09f1e360ef6603d0cfcdac1200b79bbce70bb348
Deep Patel <deepgpatel93@gmail.com>
2018-02-07 20:33:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var katzDeli = [];
 
 function takeANumber(katzDeli, newPerson){
-  var katzDeliLine = [katzDeli, ...newPerson];
-  var i = 0;
-  for (i = 0; i < katzDeliLine.length; i++) {
-    katzDeliLine.push("Welcome,"+ newPerson +". You are number"+ katzDeliLine.length  +"in line.")
-  }
+  var katzDeliLine = [katzDeli];
+  katzDeliLine.push
+  // var i = 0;
+  // //for (i = 0; i < katzDeliLine.length; i++) {
+  //   katzDeliLine.push("Welcome,"+ newPerson +". You are number"+ katzDeliLine.length  +"in line.")
+  // }
 return katzDeliLine
 }
 
