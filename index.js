92fe2f703a4b72063e7411d3def5a3fb4cd90479
Deep Patel <deepgpatel93@gmail.com>
2018-02-07 21:55:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ return "Welcome, "+ newPerson +". You are number "+ katzDeli.length +" in line."
 
 function nowServing(katzDeli){
  if (katzDeli.length > 0){
-   return `Currently serving ${katzDeli.shift()}.`  
+   return `Currently serving ${katzDeli.shift()}.` ; 
  }
  else {
-   return "There is nobody waiting to be served!"
+   return "There is nobody waiting to be served!";
  }
-return console.log(katzDeli)
+return console.log(katzDeli);
 }
 
 function currentLine(katzDeli){
-  var i = 0
+  var i = 0;
   for (i = 0; i < katzDeli.length; i++) {
     if (katzDeli.length > 0){
-    return `The line is currently: ${i+1}. ${katzDeli.shift()},`
+    return `The line is currently: ${i+1}. ${katzDeli.shift()},`;
     }
     else {
-      return "The line is currently empty."
+      return "The line is currently empty.";
     }
   }
-return console.log(katzDeli)
+return console.log(katzDeli);
 }
 
 
