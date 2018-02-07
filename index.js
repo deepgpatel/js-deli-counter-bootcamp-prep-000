cb634cdeb542a88a7da6a0e8eb33d653f80f3db1
Deep Patel <deepgpatel93@gmail.com>
2018-02-07 20:25:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, newPerson){
+  var katzDeliLine = [katzDeli, ...newPerson];
+  var i = 0;
+  for (i = 0; i < katzDeliLine.length; i++) {
+    katzDeliLine.push("Welcome,"+ newPerson +". You are number"+ katzDeliLine.length  +"in line.")
+  }
+return katzDeliLine
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
