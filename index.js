361039795d74ca644aad4154eed5822064dfd480
Deep Patel <deepgpatel93@gmail.com>
2018-02-07 20:58:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ return "Welcome, "+ newPerson +". You are number "+ katzDeli.length +" in line."
 }
 
 function nowServing(katzDeli,newPerson){
-  if (hour < 18) {
-    greeting = "Good day";
-   else {
-    greeting = "Good evening";
-}
+ if 
 }
 
 
