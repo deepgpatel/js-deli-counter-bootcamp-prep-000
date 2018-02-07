6dfe65a2346059ffd51ba569e1b4316e81b77b8b
Deep Patel <deepgpatel93@gmail.com>
2018-02-07 21:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ return "Welcome, "+ newPerson +". You are number "+ katzDeli.length +" in line."
 
 function nowServing(katzDeli,newPerson){
  katzDeli.push(newPerson)
- if (katzDeli )
+ if (newPerson){
+   
+ }
 }
 
 
