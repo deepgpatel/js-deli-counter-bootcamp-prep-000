0f7af6f5dcb42977783c98918217b6e459e3bf34
Deep Patel <deepgpatel93@gmail.com>
2018-02-07 21:45:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(katzDeli){
   var i = 0
   for (i = 0; i < katzDeli.length; i++) {
     if (katzDeli.length > 0){
-    return `The line is currently: ${[i+1]}. ${katzDeli.shift()},`
+    return `The line is currently: ${i+1}. ${katzDeli.shift()},`
     }
     else {
       return "The line is currently empty."
